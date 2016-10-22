0265e29c736f66721d0955fd629788df6a33afb1
quantumke <nguruben@gmail.com>
2016-10-22 20:41:20 +0300

    bare bones setup

--- index.py
+++ index.py
@@ -25,5 +25,33 @@ class GuiInit(tkinter.Tk):
         frame=self.frames[count]
         frame.tkraise()
 
+class XavierFace(ttk.Frame):
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+        self.FaceFeatures()
+    def FaceFeatures(self):
+        self.grid(column=1, row=1, sticky='nsew')
+        self.status = ttk.LabelFrame(self, text='My Response', height=100)
+        self.status.grid(column=0, row=4, columnspan=4, sticky='nesw')
+
+        self.status_label = ttk.Label(self.status, text='',font=LARGE_FONT)
+        self.status_label.grid(column=1, row=2)
+
+        ttk.Label(self, text='I am Xavier',font=LARGE_FONT).grid(column=0, row=0,  columnspan=4)
+        ttk.Separator(self, orient='horizontal').grid(column=0, row=1, columnspan=4, sticky='ew')
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+    def salutations(self):
+        a= dt.time()
+        a=str(a)
+        if a =="00:00:00":
+            self.status_label['text']="Good Morning!"
+        else:
+            self.status_label['text']="Good Evening!"
+            
+
+
+
 app=GuiInit()
 app.mainloop()
