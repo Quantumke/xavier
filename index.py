35e4247f83276e4546eac69de75562407429d4b9
quantumke <nguruben@gmail.com>
2016-10-23 23:27:52 +0300

    litsener method complete

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ import tkinter
 from tkinter import ttk
 import datetime as dt
 import time
+import PIL.Image
+from tkinter import *
+import os
 
 LARGE_FONT= ("Nexa Light", 12)
 
@@ -16,7 +19,7 @@ class GuiInit(tkinter.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames={}
-        for F in (XavierFace, me, Home):
+        for F in (XavierFace, me, Home, About):
             frame=F(container, self)
             self.frames[F]=frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -55,20 +58,44 @@ class XavierFace(ttk.Frame):
 class Home(ttk.Frame):
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Xavier says Hi,", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        self.salutations()
+        self.label = ttk.Label(self, text="Xavier says Hi,", font=LARGE_FONT)
+        self.label.pack(pady=1,padx=10)
 
         button1 = ttk.Button(self, text="About Me",
-                            command=lambda: controller.show_frame(Home))
+                            command=lambda: controller.show_frame(About))
         button1.pack()
 
         button2 = ttk.Button(self, text="How can i help",
-                            command=lambda: controller.show_frame(PageTwo))
+                            command=lambda: controller.show_frame(me))
         button2.pack()
+  
+    def salutations(self):
+        self.label = ttk.Label(self, text="Hi", font=LARGE_FONT)
+        self.responses= ttk.Label(self, text="", font=LARGE_FONT)
+        self.label.pack(pady=1,padx=1)
+        self.responses.pack(pady=15,padx=15)
+        a= dt.datetime.now().strftime('%H')
+        a=int(a)
+        if a < 12:
+            self.label['text']="Good Morning!"
+        else:
+            self.label['text']="Good Evening!"
 
 
         
-        
+class About(ttk.Frame):
+     def __init__(self, parent, controller):
+         ttk.Frame.__init__(self, parent)
+         self.label = ttk.Label(self, text="Xavier says Hi,", font=LARGE_FONT)
+         self.label.pack(pady=10,padx=10)
+         label = ttk.Label(self, text="Xavier is a neualnetwork,", font=LARGE_FONT)
+         label.pack(pady=10,padx=100)
+         button1 = ttk.Button(self, text="Back Home",
+                            command=lambda: controller.show_frame(Home))
+         button1.pack()
+         
+         
 
 
 
@@ -84,6 +111,8 @@ class me(ttk.Frame):
            self.responses= ttk.Label(self, text="", font=LARGE_FONT)
            self.label.pack(pady=10,padx=10)
            self.responses.pack(pady=15,padx=15)
+           litsen_button=ttk.Button(self, text="Litsen",command=self.butler)
+           litsen_button.pack()
     def salutations(self):
         a= dt.datetime.now().strftime('%H')
         a=int(a)
@@ -92,8 +121,22 @@ class me(ttk.Frame):
         else:
             self.label['text']="Good Evening!"
         
+    def butler(self):
+        import speech_recognition as sr
+        r=sr.Recognizer()
+        with sr.Microphone() as source:
+            self.label['text']="Speak out Your Command!"
+            audio=r.listen(source)
+
+        try:
+            self.label['text']=r.recognize_google(audio)
+        except sr.UnknownValueError:
+            self.label['text']="Sorry i cant understand you!"
+        except sr.RequestError as e:
+            self.label['text']=e
+        #self.butler()
         
-   
+        
             
             
         
